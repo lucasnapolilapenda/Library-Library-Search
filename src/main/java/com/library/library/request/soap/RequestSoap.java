ea37d3dc835e05e4e3c8d617a12ef6b529870626
Lucas Napoli <lucas.napoli@gmail.com>
2019-07-31 18:24:29 -0400

    SOAP Auth Ready

--- src/main/java/com/library/library/request/soap/RequestSoap.java
+++ src/main/java/com/library/library/request/soap/RequestSoap.java
@@ -2,8 +2,14 @@ package com.library.library.request.soap;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import javax.xml.namespace.QName;
+import javax.xml.ws.BindingProvider;
 import javax.xml.ws.Service;
+import javax.xml.ws.handler.MessageContext;
 
 public class RequestSoap {
 
@@ -12,6 +18,14 @@ public class RequestSoap {
         QName qname = new QName("http://publisher.search.library.com/", "PublisherImpService");
         Service service = Service.create(url, qname);
         PublisherInterface publisher = service.getPort(PublisherInterface.class);
+
+        Map<String, Object> requestContext = ((BindingProvider)publisher).getRequestContext();
+        requestContext.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
+                "http://localhost:8092/publishers/publisher?wsdl");
+        Map<String, List<String>> requestHeaders = new HashMap<String, List<String>>();
+        requestHeaders.put("username", Collections.singletonList("book-service"));
+        requestHeaders.put("Password", Collections.singletonList("book-service-secure-password-1234"));
+        requestContext.put(MessageContext.HTTP_REQUEST_HEADERS, requestHeaders);
 
         Publisher publisher1 = new Publisher();
         publisher1.setPublisherName(publisherName);
